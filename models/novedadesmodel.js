742ea3307191aecaa7dd3ccbcd3de6ec19c05f09
Maia I Elias <maiaineselias@gmail.com>
2021-06-21 22:41:23 -0300

    agregar y eliminar

--- models/novedadesmodel.js
+++ models/novedadesmodel.js
@@ -1,9 +1,34 @@
 var pool = require('./bd');
 
+
+// para seleccionar todas las novedades
+
 async function getNovedades() {
     var query = 'select * from novedades';
     var rows = await pool.query(query);
     return rows;
 }
 
-module.exports = { getNovedades }
+
+// para agregar las novedades
+
+async function insertNovedad(obj) {
+    try {
+        var query = 'insert into novedades set ?'
+        var rows = await pool.query(query,[obj]);
+        return rows;
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
+
+
+// para eliminar novedades
+async function deleteNovedadById(obj){
+    var query = 'delete from novedades where id = ?';
+    var rows = await pool.query(query,[obj]);
+    return rows;
+};
+
+module.exports = { getNovedades, insertNovedad, deleteNovedadById }
--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -4,25 +4,60 @@ var novedadesModel = require('../../models/novedadesmodel')
 
 
 //página principal de novedades
-router.get('/', async function (req,res,next) {
- var novedades = await novedadesModel.getNovedades();
+router.get('/', async function (req, res, next) {
+    var novedades = await novedadesModel.getNovedades();
 
-   res.render('admin/novedades',{
+    res.render('admin/novedades', {
         layout: 'admin/layout',
-        usuario:req.session.nombre, 
+        usuario: req.session.nombre,
         novedades
     });
-    
+
 });
 
 
 //página agregar novedades
-router.get('/agregar', function(req,res,nexs){
-res.render('admin/agregar',{
-    layout:'admin/layout'
-});
+router.get('/agregar', function (req, res, next) {
+    res.render('admin/agregar', {
+        layout: 'admin/layout'
+    });
 });
 
+
+//insertar novedad, procesa lo del formulario
+router.post('/agregar', async (req, res, next) => {
+    try {
+        var titulo = req.body.titulo;
+        var autor = req.body.autor;
+        var cuerpo = req.body.cuerpo;
+
+        if (titulo != "" && autor != "" && cuerpo != "") {
+            await novedadesModel.insertNovedad(req.body);
+            res.redirect('/admin/novedades');
+
+        } else {
+            res.render('admin/agregar', {
+                layout: 'admin/layout',
+                error: true,
+                message: 'Todos los campos son requeridos'
+            });
+        }
+    } catch (error) {
+        console.log(error);
+        res.render('admin/agregar', {
+            layout: 'admin/layout',
+            error: true,
+            message: 'No se cargó la novedad'
+        })
+    }
+});
+
+// para eliminar una novedad
+router.get('/eliminar/:id', async(req, res, next)=>{
+    var id = req.params.id;
+    await novedadesModel.deleteNovedadById(id);
+    res.redirect('/admin/novedades')
+});
 
 
 
