11f0abce56e61024821d99045ed8671091f6eeeb
Maia I Elias <maiaineselias@gmail.com>
2021-07-07 23:41:04 -0300

    preview file

--- public/javascripts/novedades.js
+++ public/javascripts/novedades.js
@@ -25,3 +25,14 @@ else {
 };
 
 
+function previewImage() {        
+    var reader = new FileReader();         
+    reader.readAsDataURL(document.getElementById('uploadImage').files[0]);         
+    reader.onload = function (e) {             
+        document.getElementById('uploadPreview').src = e.target.result;         
+    };     
+}
+
+
+
+
