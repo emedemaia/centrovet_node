534a803c9ca0f1bcad164b6df11a53ebbeccb439
Maia I Elias <maiaineselias@gmail.com>
2021-07-09 19:48:21 -0300

    agregar y modificar novedades con img y sin img ok

--- public/javascripts/novedades.js
+++ public/javascripts/novedades.js
@@ -3,39 +3,54 @@ var hora = fecha.getHours();
 
 // console.log(hora);
 
-function horario(hora){
-    if (hora >=6 && hora < 12) {
+function horario(hora) {
+    if (hora >= 6 && hora < 12) {
 
-    document.write('¡Buen día, ');
-}
-
-else {
-
-    if (hora >=12 && hora <= 19) {
-
-        document.write('¡Buenas tardes, ');
+        document.write('¡Buen día, ');
     }
 
     else {
 
-        document.write('¡Buenas noches, ');
+        if (hora >= 12 && hora <= 19) {
 
+            document.write('¡Buenas tardes, ');
+        }
+
+        else {
+
+            document.write('¡Buenas noches, ');
+
+        }
     }
-}
 };
 
 
-function previewImage() {        
-    var reader = new FileReader();         
-    reader.readAsDataURL(document.getElementById('uploadImage').files[0]);         
-    reader.onload = function (e) {             
-        document.getElementById('uploadPreview').src = e.target.result;  
-        document.getElementById('imgPreview').value = e.target.result;         
-    };     
+function previewImage() {
+    var reader = new FileReader();
+    var fileToRead = document.getElementById('uploadImage').files[0];
+
+    
+    reader.onload = function (e) {
+        document.getElementById('uploadPreview').src = e.target.result;
+    };
+
+    reader.readAsDataURL(fileToRead);
+    console.log(fileToRead)
+
+
+    var inputImage = document.getElementById('imgPreview');
+    var att = document.createAttribute('value');
+    att.value = "imagen";
+    inputImage.setAttributeNode(att);
+
 };
 
 
- 
- 
+
+
+
+
+
+
 
 
